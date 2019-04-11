b8a4866da1d6a6e062c706467eaed6de866f2166
kadieee <kadietruong16@yahoo.com>
2019-04-10 17:24:20 -0700

    deckout index added

--- src/games/deckout/Deckout.js
+++ src/games/deckout/Deckout.js
@@ -1,23 +1,25 @@
 import GameComponent from '../../GameComponent.js';
 import React from 'react';
+import UserApi from '../../UserApi.js';
 
 export default class DeckOut extends GameComponent {
 
-  render() {
-    var id = this.getSessionId();
-    var users = this.getSessionUserIds().map((user_id) => (
-      <li key={user_id}>{user_id}</li>
-    ));
-    var creator = this.getSessionCreatorUserId();
-    return (
-      <div>
-        <p>Session ID: {id}</p>
-        <p>Session creator: {creator}</p>
-        <p>Session users:</p>
-        <ul>
-          {users}
-        </ul>
-      </div>
-    );
+   render() {
+      console.log(this.getSessionDatabaseRef());
+      var id = this.getSessionId();
+      var users = this.getSessionUserIds().map((user_id) => (
+        <li key={user_id}>{UserApi.getName(user_id)}</li>
+      ));
+      var creator = UserApi.getName(this.getSessionCreatorUserId());
+      return (
+        <div>
+          <p>Session ID: {id}</p>
+          <p>Session creator: {creator}</p>
+          <p>Session users:</p>
+          <ul>
+            {users}
+          </ul>
+        </div>
+      );
+    }
   }
-}
