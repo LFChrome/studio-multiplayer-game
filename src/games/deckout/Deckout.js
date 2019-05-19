e2d0739613ed474736a98429d13a54306ce23c72
unknown <wuwj2001@gmail.com>
2019-05-19 11:55:36 -0700

    minor fixes

--- src/games/deckout/Deckout.js
+++ src/games/deckout/Deckout.js
@@ -39,8 +39,7 @@ export default class DeckOut extends GameComponent {
   }
 
   render() {
-    if (this.state) {
-      console.log(this.state);
+    if (this.state.deck) {
       return (
         <div className="container">
         <hr/>
@@ -50,7 +49,7 @@ export default class DeckOut extends GameComponent {
           <div className="row">
             <div className="col-4">{this.renderPlayerHand()}</div>
             <div className="col-8">
-              <textarea className="log" rows="10" readonly></textarea>
+              <textarea className="log" rows="10" readOnly></textarea>
             </div>
           </div>
         </div>
@@ -60,91 +59,86 @@ export default class DeckOut extends GameComponent {
       return <p>Loading</p>
     }    
   }
-
-    renderSessionInfo() {
-      var id = this.getSessionId();
-      var users = this.getSessionUserIds().map((user_id) => {
-        if (this.getSessionCreatorUserId() !== user_id) {
-          return <li key={user_id}>{UserApi.getName(user_id)}</li>
-        } else {
-          return <li key={user_id}><b>{UserApi.getName(user_id)} (Host)</b></li>
-        }
-      });
-      return (
-        <div className="row">
-          <div className="col-4">
-            <h3>
-              Deck-Out!
-            </h3>
-          </div>
-          <div className="col-4">          
-            <p>Session ID:
-              <b>{id}</b>
-            </p>
-            <p>
-              Round: {this.state.currentTurn}
-            </p>
-            <p>
-              Player Turn: {this.state.currentPlayer}
-            </p>
-          </div>
-          <div className="col-4">
-            <p>Players: {users}</p>  
-          </div>
+  renderSessionInfo() {
+    var id = this.getSessionId();
+    var users = this.getSessionUserIds().map((user_id) => {
+      if (this.getSessionCreatorUserId() !== user_id) {
+        return <li key={user_id}>{UserApi.getName(user_id)}</li>
+      } else {
+        return <li key={user_id}><b>{UserApi.getName(user_id)} (Host)</b></li>
+      }
+    });
+    return (
+      <div className="row">
+        <div className="col-4">
+          <h3>
+            Deck-Out!
+          </h3>
         </div>
-      );
-    }
-    
-    renderPlayerHand() {
-      let user = this.getMyUserId();
-      let hand = this.state.hands[user];
-      console.log(hand);
-      var hand_list = hand.map((card) => {
-        return (
-          <div className="card" key={Math.random()}>
-              <li className="list-group-item">
-                <h5 className="card-title">{card}</h5>
-                <button className={card} className="btn btn-success" onClick={() => {this.handleCardPlayed("blank")}}>Play Card</button>
-              </li>
-          </div>
-        )
-      });
-      return (
-        <ul className="list-group">
-          {hand_list}
-        </ul>
-      )
-    }
-    
-    playerTurn(){
-      if(this.state.currentTurn%2 === 0){
-        return UserApi.getName(this.getSessionCreatorUserId())
-      }else{  
-       return UserApi.getName(this.getMyUserId()) 
-      }
-    }
-
-    //Shold disable the cards on other player turn
-    oneCard(){
-      if(this.state.currentPlayer === this.getMyUserId){
-  
-      }
-    }
-
-    handleCardPlayed(card) {
-      //doCardEffect(card)
-      let currentUserHand = this.state.hands[this.getMyUserId()];
-      let indexOfCardPlayed = currentUserHand.indexOf(card);
-      currentUserHand.splice(indexOfCardPlayed, 1);
-      let newTurn = this.state.currentTurn + 1;
-      let drawDeck = this.state.deck -1;
-      this.getSessionDatabaseRef().update({
-        currentTurn: newTurn,
-        hands: {
-          [this.getMyUserId()]: currentUserHand
-        },
-        currentPlayer: this.playerTurn(),
-      });
-    }
-
+        <div className="col-4">          
+          <p>Session ID:
+            <b>{id}</b>
+          </p>
+          <p>
+            Round: {this.state.currentTurn}
+          </p>
+          <p>
+            Player Turn: {this.state.currentPlayer}
+          </p>
+        </div>
+        <div className="col-4">
+          <p>Players: {users}</p>  
+        </div>
+      </div>
+    );
   }
+  // Renders players' hands
+  renderPlayerHand() {
+    let user = this.getMyUserId();
+    let hand = this.state.hands[user];
+    var hand_list = hand.map((card) => {
+      return (
+        <div className="card" key={Math.random()}>
+            <li className="list-group-item">
+              <h5 className="card-title">{card}</h5>
+              <button className={card} className="btn btn-success" onClick={() => {this.handleCardPlayed("blank")}}>Play Card</button>
+            </li>
+        </div>
+      )
+    });
+    return (
+      <ul className="list-group">
+        {hand_list}
+      </ul>
+    )
+  }
+  
+  playerTurn(){
+    if(this.state.currentTurn%2 === 0){
+      return UserApi.getName(this.getSessionCreatorUserId())
+    }else{  
+      return UserApi.getName(this.getMyUserId()) 
+    }
+  }
+
+  //Should disable the cards on other player turn
+  oneCard(){
+    if(this.state.currentPlayer === this.getMyUserId){
+
+    }
+  }
+
+  handleCardPlayed(card) {
+    //doCardEffect(card)
+    let currentUserHand = this.state.hands[this.getMyUserId()];
+    let indexOfCardPlayed = currentUserHand.indexOf(card);
+    currentUserHand.splice(indexOfCardPlayed, 1);
+    let newTurn = this.state.currentTurn + 1;
+    let drawDeck = this.state.deck - 1;
+    this.getSessionDatabaseRef().update({
+      currentTurn: newTurn,
+      currentPlayer: this.playerTurn(),
+    });
+  }
+
+}
